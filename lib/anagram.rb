97f1fcdf87504fa7873ae5e7cf9c534d69ad1b0d
Tom Holmes <tholmes59@gmail.com>
2019-01-25 17:16:02 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -8,15 +8,9 @@ class Anagram
   end
   
   def match(words)
-    new_word = words.select do |some_word|
-      some_word.split("")
-    end
-    if new_word.sort == @word.split("").sort
-      new_word = @word
-    else
-      new_word
-    end
-    
+    words.select do |some_word|
+    some_word.split("").sort == @word.split("").sort
+  end 
   end
  
 end
